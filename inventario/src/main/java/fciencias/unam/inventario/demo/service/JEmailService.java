d04c4554eaa87ebd01ec0de0361be4bab092edba
rubenAcostaArzate <rubenacostaarzate@ciencias.unam.mx>
2023-11-27 00:38:36 -0600

    Agregando servicio para mandar correos

--- inventario/src/main/java/fciencias/unam/inventario/demo/service/JEmailService.java
+++ inventario/src/main/java/fciencias/unam/inventario/demo/service/JEmailService.java
@@ -1,8 +1,23 @@
 package fciencias.unam.inventario.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class JEmailService {
+  
+  @Autowired  
+  private JavaMailSender mailSender; 
 
     public void sendEmail(String toUser, String subject, String message){
+     SimpleMailMessage mailMessage=new SimpleMailMessage();
+     mailMessage.setFrom("rubenacostaarzate@ciencias.unam.mx");
+     mailMessage.setTo(toUser);
+     mailMessage.setSubject(subject);
+     mailMessage.setText(message);
 
+     mailSender.send(mailMessage);
+     
     }
 }
